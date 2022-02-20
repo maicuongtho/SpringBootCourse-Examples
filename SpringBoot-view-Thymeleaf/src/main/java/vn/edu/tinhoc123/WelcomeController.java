bbd9a4e94e81bc8ea908de77bed4c06a20360b20
maicuongtho <thomc@ntu.edu.vn>
2022-02-20 10:16:18 +0700

    update content

--- SpringBoot-view-Thymeleaf/src/main/java/vn/edu/tinhoc123/WelcomeController.java
+++ SpringBoot-view-Thymeleaf/src/main/java/vn/edu/tinhoc123/WelcomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class WelcomeController {
 	@GetMapping("/")
-    public String main(Model model) {
+    public String sayHello(Model model) {
         model.addAttribute("message", "Xin chào TiiL");
         return "welcome"; //view
     }
